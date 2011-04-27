ef24465ee35b9feb74649f9709cff1eab84877fc
Jeremy Kahn <jeremyckahn@gmail.com>
2011-04-27 14:58:39 -0500

    Added a jQuery plugin template.  Copy/paste for great justice.

---
+++ www/_projectname/js/closure_compiler_custom/jquery.pluginTemplate.js
@@ -0,0 +1,16 @@
+/*global jQuery: true, Modernizr:true, vsa: true */
+
+/* Author: VSA Partners
+	- Jeremy Kahn
+*/
+
+(function( $ ){
+	
+	$.fn.plugin = function() {
+		
+		return this.each(function (index, el) {
+			
+			el = $(el);
+		});
+	};
+}( jQuery ));
