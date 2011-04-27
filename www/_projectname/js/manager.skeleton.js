f6a23b80b1185d4be8ff4983bc6b7ee9b85d00bb
Jeremy Kahn <jeremyckahn@gmail.com>
2011-04-27 14:28:16 -0500

    Added a Manager skeleton for super EZ copy/pasting.

---
+++ www/_projectname/js/manager.skeleton.js
@@ -0,0 +1,31 @@
+/*global console:true, window:true, jQuery:true, Modernizr:true, vsa:true */
+
+/*******************************************************
+VSA Partners - Manager Module
+
+Dependencies: jQuery, Modernizr, vsa.js
+*******************************************************/
+
+(function skeletonManager () {
+	var self;
+
+	vsa.manager('hash', {
+		init: function() {
+			self = this;
+		},
+
+		bind: function() {
+			
+		}
+	}, {
+		// Private methods
+		_privateMethod: function _privateMethod () {
+
+		}
+	}, {
+		// Public methods
+		publicMethod: function publicMethod (ev, el) {
+
+		}
+	});
+}());
